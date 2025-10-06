7a0bcaa402adae1c2bda356e0d7589d3c9dcba8b
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-10-06 19:59:41 -0300

    feat: first change

--- guia-turistico/App.js
+++ guia-turistico/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Olá, Mundo! Primeiro App Guia Turístico da Yas :)</Text>
       <StatusBar style="auto" />
     </View>
   );
