46abc8c529b0089361d142c32336e2fff79a6368
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-10-07 19:58:40 -0300

    feat: add context to App

--- guia-turistico/App.js
+++ guia-turistico/App.js
@@ -4,25 +4,28 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import ListaPontosTuristicos from "./screens/ListaPontosTuristicos";
 import DetalhesPontoTuristico from "./screens/DetalhesPontoTuristico";
+import { FavoritesProvider } from "./context/FavoritesContext";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="ListaPontos">
-        <Stack.Screen
-          name="ListaPontos"
-          component={ListaPontosTuristicos}
-          options={{ title: "Pontos Turísticos" }}
-        />
+    <FavoritesProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="ListaPontos">
+          <Stack.Screen
+            name="ListaPontos"
+            component={ListaPontosTuristicos}
+            options={{ title: "Pontos Turísticos" }}
+          />
 
-        <Stack.Screen
-          name="DetalhesPonto"
-          component={DetalhesPontoTuristico}
-          options={{ title: "Detalhes do Ponto" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+          <Stack.Screen
+            name="DetalhesPonto"
+            component={DetalhesPontoTuristico}
+            options={{ title: "Detalhes do Ponto" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </FavoritesProvider>
   );
 }
