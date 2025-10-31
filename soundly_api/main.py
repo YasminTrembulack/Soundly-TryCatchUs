b05195ea45b165a72274adb195f412536708b74c
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-10-31 19:18:29 -0300

    feat: add / route to soundly api

--- soundly_api/main.py
+++ soundly_api/main.py
@@ -5,6 +5,8 @@ from fastapi import FastAPI, Depends, HTTPException, Header, Query
 from sqlalchemy.orm import Session
 from typing import List
 
+import uvicorn
+
 from database import SessionLocal, engine, Base
 from models import Track, Album, Artist
 from schemas import TrackSchema, AlbumSchema, ArtistSchema
@@ -36,7 +38,21 @@ def get_db():
 # 🔹 ENDPOINTS
 # ==============================
 
-@app.get("/tracks/", response_model=List[TrackSchema])
+@app.get("/", tags=["Root"])
+def index():
+    return {
+        "app_name": "Soundly API",
+        "version": "1.0.0",
+        "description": "API para gerenciar artistas, álbuns e músicas tipo Spotify",
+        "endpoints": {
+            "/tracks": "Listagem de músicas com paginação",
+            "/albums": "Listagem de álbuns com paginação e imagens",
+            "/artists": "Listagem de artistas com paginação"
+        },
+        "message": "Bem-vindo à Soundly API 🎵"
+    }
+
+@app.get("/tracks", response_model=List[TrackSchema])
 def list_tracks(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     tracks = db.query(Track).offset(skip).limit(limit).all()
     
@@ -47,7 +63,7 @@ def list_tracks(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return tracks
 
 
-@app.get("/albums/", response_model=List[AlbumSchema])  # inclui orm_mode
+@app.get("/albums", response_model=List[AlbumSchema])  # inclui orm_mode
 def list_albums(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     albums = db.query(Album).offset(skip).limit(limit).all()
     
@@ -59,7 +75,14 @@ def list_albums(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return albums
 
 
-@app.get("/artists/", response_model=List[ArtistSchema])
+@app.get("/artists", response_model=List[ArtistSchema])
 def list_artists(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     artists = db.query(Artist).offset(skip).limit(limit).all()
     return artists
+
+
+if __name__ == "__main__":
+    host = "127.0.0.1"
+    port = 8000
+    print(f"🚀 API rodando em: http://{host}:{port}")
+    uvicorn.run("main:app", host=host, port=port, reload=True)
