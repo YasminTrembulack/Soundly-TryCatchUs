1d0a5ff7e6a336cca856b2b268a9fca533dc3057
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-11-19 20:00:32 -0300

    feat: change baseURL and remove unused file

--- soundly/src/services/api.js
+++ soundly/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://soundly-api.onrender.com", 
+  baseURL: "http://localhost:8000", 
   timeout: 60000,
   headers: {
     Authorization: "d0aafc97-3bdc-4b41-81fc-bc97f1fa9146",
