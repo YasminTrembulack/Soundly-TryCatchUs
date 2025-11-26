55d1e81be4d67195bfc9e4f9a9db2d89efb25986
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-11-26 20:26:19 -0300

    feat: change baseURL

--- soundly/src/services/api.js
+++ soundly/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000", 
+  // baseURL: "http://localhost:8000", 
+  baseURL: "https://soundly-api.onrender.com", 
   timeout: 60000,
   headers: {
     Authorization: "d0aafc97-3bdc-4b41-81fc-bc97f1fa9146",
